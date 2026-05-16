474077ccc7604576ff6ebfb1e7e4d0592812c405
VulcanWM <logmedha09@gmail.com>
2026-05-16 16:30:15 +0100

    feat: added a json parser which parses key value pairs

--- main.cpp
+++ main.cpp
@@ -9,22 +9,67 @@ JSONParser::JSONParser() : JSONParser("input.json") {}
 
 std::map<std::string, std::string> JSONParser::read() {
     std::ifstream file(file_name);
-    std::map<std::string, std::string> map;
+    std::map<std::string, std::string> json;
     std::string current_key;
     std::string current_value;
+    bool parsing_error = false;
+    bool parsing_finished = false;
+
     bool in_json = false;
     bool in_key = false;
     bool in_value = false;
     bool colon_seen = false;
 
-    char ch;
+    char chr;
 
-    std::cout << "Full file content:\n";
-    while (file.get(ch)) {
-        std::cout << ch;
+    while (file.get(chr) && parsing_error == false && parsing_finished == false) {
+
+        if (in_key == true) {
+            current_key.push_back(chr);
+        }
+        else if (in_value == true) {
+            current_value.push_back(chr);
+        }
+
+        if (chr == '{' and in_json == false) {
+            in_json = true;
+        }
+        else if (chr == '{' && in_json == true) {
+            parsing_error = true;
+        }
+        else if (chr == '"' && in_key == false && colon_seen == false && in_value == false) {
+            in_key = true;
+        }
+        else if (chr == '"' && in_key == true && colon_seen == false && in_value == false) {
+            current_key.pop_back();
+            in_key = false;
+        }
+        else if (chr == ':' && in_key == false && colon_seen == false && in_value == false) {
+            colon_seen = true;
+        }
+        else if (chr == ':' && colon_seen == true) {
+            parsing_error = true;
+        }
+        else if (chr == '"' && colon_seen == true && in_key == false && in_value == false) {
+            in_value = true;
+        }
+        else if (chr == '"' && colon_seen == true && in_key == false && in_value == true) {
+            current_value.pop_back();
+            in_value = false;
+        }
+        else if (chr == ',' && colon_seen == true && in_key == false && in_value == false) {
+            colon_seen = false;
+            json.insert({current_key, current_value});
+            current_key = "";
+            current_value = "";
+        }
+        else if (in_json == true && chr == '}') {
+            parsing_finished = true;
+            json.insert({current_key, current_value});
+        }
     }
 
-    return map;
+    return json;
 }
 
 int main(int argc, char* argv[]) {
@@ -34,7 +79,11 @@ int main(int argc, char* argv[]) {
     }
 
     JSONParser j(argv[1]);
-    std::map<std::string, std::string> p = j.read();
+    std::map<std::string, std::string> json = j.read();
 
+    for(const auto& elem : json)
+    {
+        std::cout << elem.first << " " << elem.second << "\n";
+    }
     return 0;
 }
